c828f44ea98b5454c7e06b5d5c39a0738a8d7e53
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-29 16:57:01 +0000

    Adds e2e tests using protractor

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,21 @@
+describe('Github Profile finder', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.getText()).toContain('spike01');
+  });
+});
